47ddf92b88c4323d322cccdcd5325abc8078d55a
Arsenalec23 <155381704+Arsenalec23@users.noreply.github.com>
2024-03-01 18:33:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ function updateCountdown() {
     var timeDifference = targetDate - currentDate;
 
     var daysLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    var hoursLeft = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesLeft = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsLeft = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     // Display the countdown
     var daysElement = document.getElementById('days');
+    var hoursElement = document.getElementById('hours');
+    var minutesElement = document.getElementById('minutes');
+    var secondsElement = document.getElementById('seconds');
+
     daysElement.innerHTML = daysLeft;
+    hoursElement.innerHTML = ('0' + hoursLeft).slice(-2);
+    minutesElement.innerHTML = ('0' + minutesLeft).slice(-2);
+    secondsElement.innerHTML = ('0' + secondsLeft).slice(-2);
 }
 
 // Update countdown every second
