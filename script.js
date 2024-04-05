0d5c6701e9ca5b0c16d0215b04892a2bb44a970b
Arsenalec23 <155381704+Arsenalec23@users.noreply.github.com>
2024-04-06 00:22:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function updateCountdown() {
 function createWeightChart() {
     // Assume you have data for the weight of the cat for each week
     const weightData = [
-	{ month: '02.03', weight: 4.4 },
-	{ month: '09.03', weight: 4.45 },    
-        { month: '16.03', weight: 4.5 },
+	{ month: '02.03', weight: 4.45 },
+	{ month: '09.03', weight: 4.5 },    
+        { month: '16.03', weight: 4.55 },
         { month: '23.03', weight: 4.6 },
 	{ month: '30.03', weight: 4.65 },
 	{ month: '06.04', weight: 4.7 }
