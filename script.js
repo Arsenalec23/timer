00db4809c9bdb74a5da2729fcf68387b1a21a02e
Arsenalec23 <155381704+Arsenalec23@users.noreply.github.com>
2024-06-01 12:53:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function updateCountdown() {
     // Target date: May 27, 2024
-    var targetDate = new Date('2024-06-01T09:30:00').getTime();
+    var startDate = new Date('2024-06-01T09:30:00').getTime();
 
     var currentDate = new Date().getTime();
 
-    var timeDifference = targetDate - currentDate;
+    var timeDifference = currentDate - startDate;
 
     var daysLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     var hoursLeft = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -17,10 +17,10 @@ function updateCountdown() {
     var minutesElement = document.getElementById('minutes');
     var secondsElement = document.getElementById('seconds');
 
-    daysElement.innerHTML = daysLeft;
-    hoursElement.innerHTML = ('0' + hoursLeft).slice(-2);
-    minutesElement.innerHTML = ('0' + minutesLeft).slice(-2);
-    secondsElement.innerHTML = ('0' + secondsLeft).slice(-2);
+    daysElement.innerHTML = daysElapsed;
+    hoursElement.innerHTML = ('0' + hoursElapsed).slice(-2);
+    minutesElement.innerHTML = ('0' + minutesElapsed).slice(-2);
+    secondsElement.innerHTML = ('0' + secondsElapsed).slice(-2);
 }
 
 // Function to create the weight chart
